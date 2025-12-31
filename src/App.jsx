f30a10d4d9eefa82c85dcc0b035075345022c931
bhavyaagarwal25 <bhavyaagarwal947@gmail.com>
2025-12-31 14:09:29 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 // import Reeact from "react";
 import AdminDashboard from "./pages/AdminDashboard";
 function App(){
-    return <AdminDashboard />;
+     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl text-blue-500 font-bold underline bg-blue-700 p-4">
+        Hello world!
+      </h1>
+    </div>
+    //return <AdminDashboard />;
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,5 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
         <App/>
-  
 );
 
